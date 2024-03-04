45f7f3a0a12c8b86526571afe2982cec49237399
Luiz Pedro Galdino Silva <luispedrogaldino53@gmail.com>
2024-03-03 23:21:48 -0300

    💄feat: Global style creation

---
+++ src/Styles/global.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: 'poppins', sans-serif;
+  }
+
+  body {
+    width: 100vh;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    background-color: #f2f2f2;
+  }
+`;
+
+export default Global;
